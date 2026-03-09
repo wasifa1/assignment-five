eacc64ce664197ddcf0838f36b0aa82a597fef45
wasifa1 <wasifafatima123@gmail.com>
2026-03-09 06:35:00 +0600

    Login section implemented and logging enabled

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Loggin in with correct credentials: 
+const loginSection = document.getElementById("login-section");
+const mainSection = document.getElementById("main-section");
+document.getElementById("login-btn")
+.addEventListener("click", () => {
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    if(username.value === "admin" && password.value === "admin123"){
+        loginSection.classList.add("hidden");
+        mainSection.classList.remove("hidden");
+    }
+    else{
+        alert("Wrong username or password");
+        username.value = "";
+        password.value = "";
+    }
+});
+
 
